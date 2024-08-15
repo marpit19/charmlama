1aa5c235b4eea569ad6a115c4ac07222351b27f6
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2024-08-15 22:30:52 +0530

    added model selection functionality

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/marpit19/charmlama
 
 go 1.22.1
 
+require github.com/charmbracelet/bubbles v0.18.0
+
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/bubbletea v0.26.6 // indirect
 	github.com/charmbracelet/harmonica v0.2.0 // indirect
@@ -19,8 +22,10 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sahilm/fuzzy v0.1.1-0.20230530133925-c48e322e2a8f // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
---
+++ internal/models/selector.go
@@ -0,0 +1,96 @@
+package models
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+type ModelSelector struct {
+	list     list.Model
+	choice   string
+	quitting bool
+}
+
+func NewModelSelector(models []string) *ModelSelector {
+	items := make([]list.Item, len(models))
+	for i, model := range models {
+		items[i] = item{
+			title: model,
+			desc:  fmt.Sprintf("Ollama model: %s", model),
+		}
+	}
+
+	l := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	l.Title = "Select an Ollama Model"
+
+	return &ModelSelector{
+		list: l,
+	}
+}
+
+func (m *ModelSelector) Init() tea.Cmd {
+	return nil
+}
+
+func (m *ModelSelector) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		case "enter":
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.choice = i.title
+			}
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		h, v := appStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m *ModelSelector) View() string {
+	if m.choice != "" {
+		return fmt.Sprintf("You chose %s\n", m.choice)
+	}
+	if m.quitting {
+		return "Bye!\n"
+	}
+	return appStyle.Render(m.list.View())
+}
+
+var appStyle = lipgloss.NewStyle().Padding(1, 2)
+
+func SelectModel(models []string) (string, error) {
+	m := NewModelSelector(models)
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	model, err := p.Run()
+	if err != nil {
+		return "", fmt.Errorf("error running model selector: %w", err)
+	}
+
+	if m, ok := model.(*ModelSelector); ok {
+		return m.choice, nil
+	}
+
+	return "", fmt.Errorf("could not get selected model")
+}
